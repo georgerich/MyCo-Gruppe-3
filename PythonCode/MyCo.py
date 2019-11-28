9a265b4591dfda872e3447d4df3a67787b80b158
Michael Schulz PC <michael.s.schulz@gmx.de>
2019-11-28 12:11:18 +0100

    Detect and give out paramaters send from Dialogflow

--- PythonCode/MyCo.py
+++ PythonCode/MyCo.py
@@ -1,13 +1,16 @@
 # import flask dependencies
 from flask import Flask, request, make_response, jsonify
+from datetime import datetime
 
 # initialize the flask app
 app = Flask(__name__)
+
 
 # default route
 @app.route('/')
 def index():
     return 'Hello World!'
+
 
 # function for responses
 def results():
@@ -18,9 +21,21 @@ def results():
     action = req.get('queryResult').get('action')
     print(action)
     print(req)
-    # return a fulfillment response
-    return {'fulfillmentText': 'This is a response from webhook.'}
+    topic = req.get('queryResult').get('parameters').get('Topic')
+    geocountry = req.get('queryResult').get('parameters').get('geo-country')
+    subtopic = req.get('queryResult').get('parameters').get('Subtopic')
+    topic1 = req.get('queryResult').get('parameters').get('Topic1')
+    timestamp = datetime.now()
 
+    print('topic: ' + topic)
+    print('geo-location:' + geocountry)
+    print('Subtopic: ' + subtopic)
+    print('topic 1: ' + topic1)
+    print('timestamp: ' + str(timestamp))
+
+    # return a fulfillment response
+    return {
+        'fulfillmentText': 'you where looking for topic: ' + topic + '\n and topic 1: ' + topic1 + '\n and Subtopic: ' + subtopic + ' in: ' + geocountry}
 
 
 # create a route for webhook
@@ -29,6 +44,7 @@ def webhook():
     # return response
     return make_response(jsonify(results()))
 
+
 # run the app
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
