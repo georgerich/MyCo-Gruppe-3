0c38b2d0335f62c14cb47f644d7bd5f0909b44a2
Michael Schulz PC <michael.s.schulz@gmx.de>
2019-11-28 10:12:13 +0100

    Create Webhook with flask

---
+++ PythonCode/MyCo.py
@@ -0,0 +1,34 @@
+# import flask dependencies
+from flask import Flask, request, make_response, jsonify
+
+# initialize the flask app
+app = Flask(__name__)
+
+# default route
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+# function for responses
+def results():
+    # build a request object
+    req = request.get_json(force=True)
+
+    # fetch action from json
+    action = req.get('queryResult').get('action')
+    print(action)
+    print(req)
+    # return a fulfillment response
+    return {'fulfillmentText': 'This is a response from webhook.'}
+
+
+
+# create a route for webhook
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    # return response
+    return make_response(jsonify(results()))
+
+# run the app
+if __name__ == '__main__':
+   app.run(debug=True)
